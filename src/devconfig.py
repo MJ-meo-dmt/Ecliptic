3c937525a1c40e48989e4b4549a1ff4cc64c25b8
Martin de Bruyn <francois.debruyn66@gmail.com>
2012-05-18 08:43:14 +0200

    Added a small py file for quick configs

---
+++ src/devconfig.py
@@ -0,0 +1,20 @@
+#######################
+#######################
+#
+#  Quick code config
+#
+#######################
+#######################
+
+
+# Camera settings
+camFov = 70
+camNear = .1
+camFar = 1000
+
+
+# Player settings
+PCSpeed = 4
+
+# Player Physics settings
+# Will add them later since its still a bit crazy in physics.py
