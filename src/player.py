1c3922675544b11e88bc9dd2e6d43b2a91744647
Martin de Bruyn <francois.debruyn66@gmail.com>
2012-05-21 09:46:14 +0200

    Removed unwanted code

--- src/player.py
+++ src/player.py
@@ -69,6 +69,7 @@ class MakePlayer(Player):
                 
 	# Setup Player inventory
 	self.playerDataStorage = [] # May change
+	
                 
 	# Setup the Player
 	self.entityPlayer = render.attachNewNode(entityName)
@@ -113,99 +114,11 @@ class PlayerInput(DirectObject):
     PlayerInput Class:
         
     Handles all the Inputs from the "PC" aka Player
+    Note: Physics.py handles all PC related movement aka, keyboard+mouse
         
     """     
-    def __init__(self, mp):
+    def __init__(self):
 	
-	# From main - Main physics
-	self.mp = mp
-	
-	# Get the screen size for the camera controller
-	self.winXhalf = base.win.getXSize()/2 
-	self.winYhalf = base.win.getYSize()/2
-                
-	# Reparent the camera to the player
-	base.camera.reparentTo(ENTITY['PC'].entityPlayer) 
-	base.camera.setPos(0,0,1.7) 
-	base.camLens.setNearFar(camNear,camFar)
-	base.camLens.setFov(camFov) 
-	base.disableMouse() 
-                
-	# Set the control maps.
-	self.controlMap = {"left": 0, "right": 0, "forward": 0, "backward": 0, "jump": 0, "wheel-in": 0, "wheel-out": 0}
-                
-	# Add the move method to the taskman
-	taskMgr.add(self.move, 'move-task')
-                
 	### SETUP KEYBOARD ###
 	self.accept( "escape",sys.exit ) 
-	'''
-	self.accept("w", self.setControl, ["forward", 1])
-	self.accept("a", self.setControl, ["left", 1])
-	self.accept("s", self.setControl, ["backward", 1])
-	self.accept("d", self.setControl, ["right", 1])
-	self.accept("space", self.setControl, ["jump", 1])
-                
-	self.accept("w-up", self.setControl, ["forward", 0])
-	self.accept("a-up", self.setControl, ["left", 0])
-	self.accept("s-up", self.setControl, ["backward", 0])
-	self.accept("d-up", self.setControl, ["right", 0])
-	self.accept("space-up", self.setControl, ["jump", 0])
-        
-    def setControl(self, key, value):
-	self.controlMap[key] = value
-        
-    # This is for calculating the jump.
-    def jump(self, dt):
-	# Get the "floating" from player object. 
-	self.playerFloating = ENTITY['PC'].floating
-	# Get the player - PC
-	self.playerPc = ENTITY['PC'].entityPlayer
-                
-	if not self.playerFloating:
-		self.playerFloating=True
-		lf=LerpFunc(lambda z: self.playerPc.setZ(z),
-			fromData = self.playerPc.getZ(),
-			toData = self.playerPc.getZ()+1.0, duration = 0.7,
-			blendType = 'easeOut')
-		self.seq=Sequence(lf, Wait(.7))
-		self.seq.start()
-	elif not self.seq.isPlaying(): self.playerFloating=False
-        '''
-    def move(self, task):
-                
-	# Get the player - PC
-	self.player = self.mp.physics.actorNP
-	# Get the player speed
-	self.playerSpeed = ENTITY['PC'].playerSpeed
-                
-	dt=globalClock.getDt() 
-                
-	# Handle mouse
-	md = base.win.getPointer(0) 
-	x = md.getX() 
-	y = md.getY() 
-	if base.win.movePointer(0, self.winXhalf, self.winYhalf): 
-		self.player.setH(self.player, (x - self.winXhalf)*-0.1) 
-		base.camera.setP( clampScalar(-90,90, base.camera.getP() - (y - self.winYhalf)*0.1) ) 
 	
-	'''
-	# Move the player if the keys are pressed 
-	# Forward.
-	if (self.controlMap["forward"] != 0):
-	    self.player.setY(self.player, self.playerSpeed * globalClock.getDt())
-	# Backward.
-	if (self.controlMap["backward"] != 0):
-	    self.player.setY(self.player, -self.playerSpeed * globalClock.getDt())
-	# Left.
-	if (self.controlMap["left"] != 0):
-	    self.player.setX(self.player, -self.playerSpeed * globalClock.getDt())
-	# Right.
-	if (self.controlMap["right"] != 0):
-	    self.player.setX(self.player, self.playerSpeed * globalClock.getDt())
-	# Jump
-	if (self.controlMap["jump"] != 0):
-	    self.jump(dt)
-	'''
-                         
-	return task.cont         
