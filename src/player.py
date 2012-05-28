e2f2b5dec0ce45919674ac883890c68554629eb6
Martin de Bruyn <francois.debruyn66@gmail.com>
2012-05-28 15:02:40 +0200

    Added the player bullet and inputs

--- src/player.py
+++ src/player.py
@@ -10,6 +10,11 @@ from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import *
 from direct.task import Task
 from direct.showbase.DirectObject import DirectObject
+from panda3d.core import BitMask32
+from panda3d.bullet import ZUp
+from panda3d.bullet import BulletCapsuleShape
+from panda3d.bullet import BulletCharacterControllerNode
+from direct.showbase.InputStateGlobal import inputState
 
 # Game imports
 from devconfig import *
@@ -31,6 +36,7 @@ class Player(object):
     def __init__(self): 	    
 	pass
         # These are players and other entities
+	
     def addEntity(self, entityKey, entityObject):
 	"""
 	@param entityKey: Pref the name of the entity
@@ -41,7 +47,9 @@ class Player(object):
 	
 
 ## MakePlayer Class
-class MakePlayer(Player):
+
+# Will move this class under physics.py so that we have some order.
+class MakePlayer(DirectObject):
         
     """
     MakePlayer Class:
@@ -49,7 +57,7 @@ class MakePlayer(Player):
     This class handels the creation of Players.
     Players will be stored in the Entity dict.
     """
-    def __init__(self, name, entityName='PC_player'):
+    def __init__(self, _base, _physics, _world, name):
 
 	"""
 	constructor:
@@ -58,38 +66,106 @@ class MakePlayer(Player):
 	@param entityName: String_name for the PC - Player in ENTITY /
 	    dict{} for all uses in code.
 	"""
-                
+	
+	# Base class
+	self._base = _base
+	
+	# Base Physics
+	self._physics = _physics
+	
+	# Base World
+	self._world = _world
+	
 	# Setup Player name
 	self.playerName = name
-                
-	# Setup Player Move Speed
-	self.playerSpeed = PCSpeed # 
-	self.isPlayerMoving = False # This should be used with PlayerInput.
-	self.floating = False # For the jump function
                 
 	# Setup Player inventory
 	self.playerDataStorage = [] # May change
 	
-                
-	# Setup the Player
-	self.entityPlayer = render.attachNewNode(entityName)
-                
-	# Setup the spawn position
-	self.entityPlayer.setPos(0, 0, 0)
+	## ADD MOUSE LOOK TASK TO TASKMGR
+	taskMgr.add(self.mouseLook, 'camera')
 	
+	# Crouch Flag
+	self.crouching = False
 	
-## Player Input Class
-class PlayerInput(DirectObject):
-        
-    """
-    PlayerInput Class:
-        
-    Handles all the Inputs from the "PC" aka Player
-    Note: Physics.py handles all PC related movement aka, keyboard+mouse
-        
-    """     
-    def __init__(self):
+	# Mouse look
+	self.omega = 0.0
 	
-	### SETUP KEYBOARD ###
+	# Setup player input
+	self.accept('space', self.doJump)
+	self.accept('c', self.doCrouch) # We need to fix the height
 	self.accept( "escape",sys.exit ) 
+	inputState.watchWithModifiers('forward', 'w')
+	inputState.watchWithModifiers('left', 'a')
+	inputState.watchWithModifiers('reverse', 's')
+	inputState.watchWithModifiers('right', 'd')
+	inputState.watchWithModifiers('turnLeft', 'q')
+	inputState.watchWithModifiers('turnRight', 'e')
 	
+	# Camera Setup for player
+	# Get the screen size for the camera controller
+	self.winXhalf = base.win.getXSize()/2 
+	self.winYhalf = base.win.getYSize()/2
+	
+	## SETUP CHARACTER AND CHARACTER SHAPE
+	# Setup Shape
+	h = 1.75
+	w = 0.4
+	shape = BulletCapsuleShape(w, h - 2 * w, ZUp)
+
+	self.character = BulletCharacterControllerNode(shape, 0.4, 'Player')
+	self.characterNP = self._world.playerNP.attachNewNode(self.character)
+	self.characterNP.setPos(-4, 0, 1.8) # May need some tweaking
+	self.characterNP.setCollideMask(BitMask32.allOn())
+	
+	# Attach the character to the base _Physics
+	self._physics.world.attachCharacter(self.character)
+	
+	# Reparent the camera to the player
+	base.camera.reparentTo(self.characterNP) 
+	base.camera.setPos(0,0,1.7) 
+	base.camLens.setNearFar(camNear,camFar)
+	base.camLens.setFov(camFov) 
+	base.disableMouse()
+	
+	
+    # Handle player jumping
+    def doJump(self):
+	self.character.setMaxJumpHeight(2.3)
+	self.character.setJumpSpeed(4.5)
+	self.character.doJump()
+    
+    
+    # Handle player crouch.
+    def doCrouch(self):
+	self.crouching = not self.crouching
+	sz = self.crouching and 0.6 or 1.0
+
+	self.characterNP.setScale(Vec3(1, 1, sz))
+    
+    def mouseLook(self, task):
+	dt = globalClock.getDt()
+	# Handle mouse
+	md = base.win.getPointer(0) 
+	x = md.getX() 
+	y = md.getY() 
+	
+	if base.win.movePointer(0, self.winXhalf, self.winYhalf): 
+		self.omega = (x - self.winXhalf)*-mouseSpeed
+		base.camera.setP( clampScalar(-90,90, base.camera.getP() - (y - self.winYhalf)*0.1) ) 
+	self.processInput(dt)
+	return task.cont
+    
+    # Handle player input
+    def processInput(self, dt):
+	speed = Vec3(0, 0, 0)
+	
+	#@param PCSpeed: Player move speed under devconfig.py
+	if inputState.isSet('forward'): speed.setY( PCSpeed)
+	if inputState.isSet('reverse'): speed.setY(-PCSpeed)
+	if inputState.isSet('left'):    speed.setX(-PCSpeed)
+	if inputState.isSet('right'):   speed.setX( PCSpeed)
+	
+	self.character.setAngularMovement(self.omega)
+	self.character.setLinearMovement(speed, True)
+
