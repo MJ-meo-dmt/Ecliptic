f379c6894cf4fab77789e3dceeeb4034070d57e6
Martin de Bruyn <francois.debruyn66@gmail.com>
2012-05-28 15:03:16 +0200

    Added new nodepaths, also cleaned up, added parser under makelevel

--- src/world.py
+++ src/world.py
@@ -16,7 +16,7 @@ from direct.showbase.DirectObject import DirectObject
 
 # Game imports
 from globals import *
-
+from egg_parse import *
 #---------------------------------------------------------------------#
 
 ## Main World Class
@@ -30,15 +30,37 @@ class World(DirectObject):
 	
 	def __init__(self):
 		
+		# Testing light: Remove if done
+		############################################
 		# Setup lights for the level
-		
-		self.test = "hello"
-		
 		plight = PointLight('plight')
 		plight.setColor(VBase4(0.7, 0.7, 0.7, 1))
 		plnp = render.attachNewNode(plight)
 		plnp.setPos(0, 0, 8)
 		render.setLight(plnp)
+		############################################
+		
+		# Keeps all geoms under this node: Game_Objects
+		self.master_GeomNP = render.attachNewNode('MASTER_GEOM')
+		# Keeps all Lights under this node
+		self.master_LightNP = render.attachNewNode('MASTER_LIGHTS')
+		
+		# subnodes for Game_Objects
+		self.sensorNP = self.master_GeomNP.attachNewNode('SENSORS')
+		self.doorNP = self.master_GeomNP.attachNewNode('DOORS')
+		self.playerNP = self.master_GeomNP.attachNewNode('PLAYER')
+		self.suitNP = self.master_GeomNP.attachNewNode('SUIT')
+		self.triggerNP = self.master_GeomNP.attachNewNode('TRIGGERS')
+		self.itemNP = self.master_GeomNP.attachNewNode('ITEMS')
+		self.screenNP = self.master_GeomNP.attachNewNode('SCREENS')
+		self.particlesNP = self.master_GeomNP.attachNewNode('PARTICLES')
+		self.decorNP = self.master_GeomNP.attachNewNode('DECOR')
+		self.visLightsNP = self.master_GeomNP.attachNewNode('VIS_LIGHTS')
+		
+		# subnodes for lights
+		self.lightsNP = self.master_LightNP.attachNewNode('LIGHTS')
+		
+		
 		
 	def addLevel(self, levelName, levelObject):
 		"""
@@ -61,20 +83,27 @@ class MakeLevel():
 	Handles the creation of levels.
 	"""
 	
-	def __init__(self, levelName, levelModelPath):
+	def __init__(self, _base, _world, levelName, levelModelPath):
 		"""
 		constructor:
 		
 		@param levelName: String_Name for the level itself.
 		@param levelModelPath: The path to the model file, for the level
 		"""
+		# Base Class
+		self._base = _base
+		
+		# Base World
+		self._world = _world
 		
 		# Set the level name
 		self.levelName = levelName
 		
 		# Load the model for the level
-		self.dummylevel = loader.loadModel(levelModelPath)
-		self.dummylevel.reparentTo(render)
+		self.level = loader.loadModel(levelModelPath)
+		
+		# Parse the egg model file and setup the level
+		Parse(self._base, self._world, self.level)
 		
 		
 		
